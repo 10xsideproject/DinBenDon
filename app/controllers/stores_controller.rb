efedf6eefb6ae718cfab0c3d23a0904dc0f130e0
DazaWoo <darrenwu5106@gmail.com>
2022-08-20 02:02:27 +0800

    add routes resources :stores, generate model Store name, address, tel

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,31 @@
 class StoresController < ApplicationController
   def index
+  end
+  def new
+    
   end  
+  def create
+  end
+  def show
+    
+  end  
+  def edit
+    
+  end
+  def update
+    
+  end
+  def destroy
+    
+  end
+
+  private
+  def params_store
+    params.require(:store).permit(:name, :address, :tel)
+  end
+  def find_store
+    @store = Store.find(params[:id])
+  end  
+
+
 end
