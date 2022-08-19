dac670b41e0725686fd01071ccee55b3e1247e25
DazaWoo <darrenwu5106@gmail.com>
2022-08-20 02:21:45 +0800

    create Store CRUD

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,10 +1,18 @@
 class StoresController < ApplicationController
+  before_action :find_store, only: [:show, :edit, :update, :destroy]
   def index
+    @stores = Store.all
   end
   def new
-    
+    @store = Store.new
   end  
   def create
+    @store = Store.new(params_store)
+    if @store.save
+      redirect_to root_path, notice: "商店建立成功"
+    else
+      render :new
+    end   
   end
   def show
     
@@ -13,10 +21,15 @@ class StoresController < ApplicationController
     
   end
   def update
-    
+    if @store.update(params_store)
+      redirect_to root_path, notice: "商店更新成功"
+    else
+      render :edit
+    end
   end
   def destroy
-    
+    @store.destroy
+    redirect_to root_path, notice: "商店刪除成功"
   end
 
   private
