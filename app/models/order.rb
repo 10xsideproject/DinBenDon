7421d249764054fd60fd49e09b8e9d210f06e3b8
DazaWoo <darrenwu5106@gmail.com>
2022-08-22 00:32:24 +0800

    get total price issue

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :product
   validates :name, presence: true
-  before_create :get_total
+  # before_create :get_total
 
   private
-  def get_total
-    self.qunaitiy * self.product_id.price
-  end
+  # def get_total
+  #   self.qunaitiy.to_i * self.product_id.price
+  # end
 end
